c3d73821591f136278de58c76209350464dfe46a
IzzyBeraja <izzyberaja+github@gmail.com>
2025-07-09 00:35:37 -0400

    Remove unused routes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -5,10 +5,7 @@ import {
 } from "@/controllers/receipts";
 import { Get as GetUsers } from "@/controllers/users";
 import { validate } from "@/middleware/validation.middleware";
-import { getUserById } from "@/controllers/users/getUserById";
-import { createUser } from "@/controllers/users/createUser";
-import { updateUser } from "@/controllers/users/updateUser";
-import { deleteUser } from "@/controllers/users/deleteUser";
+import { NextFunction, Request, Response, Router } from "express";
 
 export const rootRouter = Router();
 
@@ -26,10 +23,6 @@ rootRouter.post("/receipts", validate(PostReceiptInput), PostReceipt);
 
 // User routes
 rootRouter.get("/users", GetUsers);
-rootRouter.get("/users/:id", getUserById);
-rootRouter.post("/users", createUser);
-rootRouter.put("/users/:id", updateUser);
-rootRouter.delete("/users/:id", deleteUser);
 
 // 404 handler for unmatched API routes
 rootRouter.use((req: Request, res: Response) => {
